66402874786bb888b2899dea66b6ff9a54bc2090
Abdulrahman Beydoun <abedbeydounaccs@gmail.com>
2024-05-03 01:32:43 +0300

    Server.js

--- Backend/Server.js
+++ Backend/Server.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const express = require('express');
 const userRoutes = require('./Routes/User');
 const schedulingRoutes = require('./Routes/Scheduling');
-const auth = require('./Middleware/Authentication');
 const cors = require('cors');
 
 
@@ -17,8 +16,7 @@ app.use((req,res,next)=>{
    console.log(req.path,req.method);
    next();
 });
- //Authentication 
-app.use(auth);
+
 
  //cors
 const options = {

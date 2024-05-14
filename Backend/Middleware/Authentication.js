d66dc9641b294bfcdad1b558e2ef5018a824f457
Mohamamd Hammoud <mohammad.hammoud.lb@hotmail.com>
2024-05-15 01:01:52 +0300

    Made schedule dynamic refactor phase 3

--- Backend/Middleware/Authentication.js
+++ Backend/Middleware/Authentication.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken')
 const User = require('../Models/userModel')
 const dotenv = require('dotenv')
+const UserModel = require('../Models/userModel')
 dotenv.config()
 module.exports = async (req, res, next) => {
     //verify authentication
@@ -16,6 +17,12 @@ module.exports = async (req, res, next) => {
         const payload = jwt.verify(token, process.env.Secret)
 
         //Find user in database and return ID
+        const doesExist = await UserModel.findById(payload?._id)
+        if (!doesExist) {
+            return res.status(404).json({
+                message: 'User was not found.',
+            })
+        }
         req.user = payload
         return next()
     } catch (error) {
--- Backend/Routes/Scheduling.js
+++ Backend/Routes/Scheduling.js
@@ -1,40 +1,40 @@
-const express = require('express');
-const router = express.Router();
-const auth = require('../Middleware/Authentication');
+const express = require('express')
+const router = express.Router()
+const auth = require('../Middleware/Authentication')
 
 //Controller Functions
 const {
-  studentInfo,
-  majorCourses,
-  electiveCourses,
-  optimizedSchedule,
-  addElectiveCourse,
-  addMajorCourse,
-  deleteCourse,
-} = require('../Controllers/scheduleController');
+    studentInfo,
+    majorCourses,
+    electiveCourses,
+    optimizedSchedule,
+    addElectiveCourse,
+    addMajorCourse,
+    deleteCourse,
+} = require('../Controllers/scheduleController')
 
 //Check authentication for routes
-router.use(auth);
+router.use(auth)
 
 //Send student info
-router.post('/studentInfo', studentInfo);
+router.post('/user-info', studentInfo)
 
 //Get available major courses
-router.get('/majorCourses', majorCourses);
+router.get('/major-courses', majorCourses)
 
 //Get available elective courses
-router.get('/electiveCourses', electiveCourses);
+router.get('/elective-courses', electiveCourses)
 
 //Get optimized schedules
-router.get('/optimizedSchedule', optimizedSchedule);
+router.get('/optimized-schedule', optimizedSchedule)
 
 //Add elective course
-router.put('/addElectiveCourse', addElectiveCourse);
+router.put('/elective-course', addElectiveCourse)
 
 //Add major course
-router.put('/addMajorCourse', addMajorCourse);
+router.put('/major-course', addMajorCourse)
 
 //Delete course
-router.delete('/deleteCourse', deleteCourse);
+router.delete('/delete-course', deleteCourse)
 
-module.exports = router;
+module.exports = router

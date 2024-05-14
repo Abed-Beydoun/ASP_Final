c3c731130e9ab9405a6ef001953731d9783ca1c8
Mohamamd Hammoud <mohammad.hammoud.lb@hotmail.com>
2024-05-14 22:32:01 +0300

    Connected register and login to backend

--- Backend/Models/userModel.js
+++ Backend/Models/userModel.js
@@ -4,55 +4,58 @@ const validator = require('validator')
 
 const Schema = mongoose.Schema
 
-const userSchema = new Schema({
-    email: {
-        type: String,
-        required: [true, 'Email is required'],
-        unique: true,
+const userSchema = new Schema(
+    {
+        email: {
+            type: String,
+            required: [true, 'Email is required'],
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: [true, 'Password is required'],
+        },
+        first_name: {
+            type: String,
+            required: [true, 'First name is required'],
+        },
+        last_name: {
+            type: String,
+            required: [true, 'Last name is required'],
+        },
+        major: {
+            type: String,
+            required: [true, 'Major is required'],
+            default: ' ',
+        },
+        finished_courses: {
+            type: [String],
+            required: [true, 'Finished courses is required'],
+            default: [],
+        },
+        days_of_unavailability: {
+            type: [String],
+            required: [true, 'Days of availability is required'],
+            default: [],
+        },
+        campus: {
+            type: String,
+            required: [true, 'Campus is required'],
+            default: ' ',
+        },
+        number_of_electives: {
+            type: Number,
+            required: [true, 'Number of elective credits is required'],
+            default: 0,
+        },
+        number_of_major: {
+            type: Number,
+            required: [true, 'Number of major credits is required'],
+            default: 0,
+        },
     },
-    password: {
-        type: String,
-        required: [true, 'Password is required'],
-    },
-    first_name: {
-        type: String,
-        required: [true, 'First name is required'],
-    },
-    last_name: {
-        type: String,
-        required: [true, 'Last name is required'],
-    },
-    major: {
-        type: String,
-        required: [true, 'Major is required'],
-        default: ' ',
-    },
-    finished_courses: {
-        type: [String],
-        required: [true, 'Finished courses is required'],
-        default: [],
-    },
-    days_of_unavailability: {
-        type: [String],
-        required: [true, 'Days of availability is required'],
-        default: [],
-    },
-    campus: {
-        type: String,
-        required: [true, 'Campus is required'],
-        default: ' ',
-    },
-    number_of_electives: {
-        type: Number,
-        required: [true, 'Number of elective credits is required'],
-        default: 0,
-    },
-    number_of_major: {
-        type: Number,
-        required: [true, 'Number of major credits is required'],
-        default: 0,
-    },
-})
+    { timestamps: true },
+)
 userSchema.pre('save', async function (next) {
     this.password = await bcrypt.hash(this.password, 10)
 })
